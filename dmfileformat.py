c836ed2ab0c18b2e41101ea399e4e5ce43d78536
shantapurmath <153751105+shantapurmath@users.noreply.github.com>
2024-01-12 10:29:31 +0000

    dmfileformat.py

--- dmfileformat.py
+++ dmfileformat.py
@@ -1,35 +1,39 @@
 import pandas as pd
 # Read the input text file into a DataFrame with space as the delimiter
 input_file_path = 'dminput1212.txt'
+output_file_path = 'output_file_DM1.txt'
 
-# define function
-df = pd.read_csv(input_file_path, delimiter=' ', skipinitialspace=True, encoding='utf-8')
+df = pd.read_csv(input_file_path, delimiter=' ', skipinitialspace=True)
+#df = pd.read_csv(input_file_path)
+print(df.columns)
+print(df)
 
 # Define the desired length for each column
+customer_no_length = 10
 branch_length = 6
 account_length = 8
 relation_type_length = 3
 
 # Conditionally add leading zeros or spaces based on the length of each column
+df['CUSTOMER_NO'] = df['CUSTOMER_NO'].apply(lambda x: str(x).zfill(customer_no_length) if len(str(x)) != customer_no_length else x)
 df['BRANCH_NO'] = df['BRANCH_NO'].apply(lambda x: str(x).zfill(branch_length) if len(str(x)) != branch_length else x)
 df['ACCOUNT_NO'] = df['ACCOUNT_NO'].apply(lambda x: str(x).zfill(account_length) if len(str(x)) != account_length else x)
 df['RELATION_TYPE'] = df['RELATION_TYPE'].apply(lambda x: str(x).ljust(relation_type_length) if len(str(x)) != relation_type_length else x)
-
 # Save the modified DataFrame to a new text file
-output_file_path = 'fileoutput1.txt'
-df.to_csv(output_file_path, sep='\t', index=False)
+
+df.to_csv(output_file_path, sep=' ', index=False, header=False)
 print(f"Processed data saved to {output_file_path}")
 
 # Replace 'your_input_file.txt' and 'your_output_file.txt' with your actual file names
-input_file = 'fileoutput1.txt'
-output_file = 'filetempout.txt'
+input_file = 'output_file_DM1.txt'
+output_file = 'intermidiateout.txt'
 
 with open(input_file, 'r') as file:
     lines = file.readlines()
 
 # Filter lines that start with a number and remove spaces
 filtered_lines = [line.strip().replace(" ", "") for line in lines if
-                  line.strip().startswith(('1', '2', '3', '4', '5', '6', '7', '8', '9'))]
+                  line.strip().startswith(('0','1', '2', '3', '4', '5', '6', '7', '8', '9'))]
 
 # Combine lines with the same key
 combined_lines = {}
@@ -46,15 +50,14 @@ df = pd.DataFrame(
 df.to_csv(output_file, index=False, header=False)
 
 # Read lines from the file
-file_path = 'filetempout.txt'  # Replace 'your_file.txt' with the actual path to your file
+file_path = 'intermidiateout.txt'  # Replace 'your_file.txt' with the actual path to your file
 with open(file_path, 'r') as file:
     lines = file.readlines()
-
 
 # Remove spaces and replace commas
 modified_lines = [line.replace(' ', '').replace(',', 'TCWCUSAC ') for line in lines]
 
 # Write the modified lines back to the file
-output_file_path = 'finaloutput_dmtool.txt'  # Replace 'output.txt' with the desired output file path
+output_file_path = 'DMTOOLFINAL_OUT.txt'  # Replace 'output.txt' with the desired output file path
 with open(output_file_path, 'w') as output_file:
     output_file.writelines(modified_lines)
