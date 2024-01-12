cea5ec563c0c1a545081f4a82f7f38bee8c83545
shantapurmath <153751105+shantapurmath@users.noreply.github.com>
2024-01-12 03:10:44 +0000

    Add files via upload

---
+++ dmfileformat.py
@@ -0,0 +1,60 @@
+import pandas as pd
+# Read the input text file into a DataFrame with space as the delimiter
+input_file_path = 'dminput1212.txt'
+
+# define function
+df = pd.read_csv(input_file_path, delimiter=' ', skipinitialspace=True, encoding='utf-8')
+
+# Define the desired length for each column
+branch_length = 6
+account_length = 8
+relation_type_length = 3
+
+# Conditionally add leading zeros or spaces based on the length of each column
+df['BRANCH_NO'] = df['BRANCH_NO'].apply(lambda x: str(x).zfill(branch_length) if len(str(x)) != branch_length else x)
+df['ACCOUNT_NO'] = df['ACCOUNT_NO'].apply(lambda x: str(x).zfill(account_length) if len(str(x)) != account_length else x)
+df['RELATION_TYPE'] = df['RELATION_TYPE'].apply(lambda x: str(x).ljust(relation_type_length) if len(str(x)) != relation_type_length else x)
+
+# Save the modified DataFrame to a new text file
+output_file_path = 'fileoutput1.txt'
+df.to_csv(output_file_path, sep='\t', index=False)
+print(f"Processed data saved to {output_file_path}")
+
+# Replace 'your_input_file.txt' and 'your_output_file.txt' with your actual file names
+input_file = 'fileoutput1.txt'
+output_file = 'filetempout.txt'
+
+with open(input_file, 'r') as file:
+    lines = file.readlines()
+
+# Filter lines that start with a number and remove spaces
+filtered_lines = [line.strip().replace(" ", "") for line in lines if
+                  line.strip().startswith(('1', '2', '3', '4', '5', '6', '7', '8', '9'))]
+
+# Combine lines with the same key
+combined_lines = {}
+for line in filtered_lines:
+    key = line[:10]  # Assuming the key is the first 10 characters
+    content = line[10:]  # Content after the key
+    combined_lines.setdefault(key, []).append(content)
+
+# Create a DataFrame with the combined lines
+df = pd.DataFrame(
+    {'key': list(combined_lines.keys()), 'content': [' '.join(contents) for contents in combined_lines.values()]})
+
+# Store the result in a new file
+df.to_csv(output_file, index=False, header=False)
+
+# Read lines from the file
+file_path = 'filetempout.txt'  # Replace 'your_file.txt' with the actual path to your file
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+
+
+# Remove spaces and replace commas
+modified_lines = [line.replace(' ', '').replace(',', 'TCWCUSAC ') for line in lines]
+
+# Write the modified lines back to the file
+output_file_path = 'finaloutput_dmtool.txt'  # Replace 'output.txt' with the desired output file path
+with open(output_file_path, 'w') as output_file:
+    output_file.writelines(modified_lines)
